4d7a811ee2a682f9fb7114fa2d46c535e45f8f7d
Tejas Patil <65388565+tejas2292@users.noreply.github.com>
2022-11-24 20:29:45 +0530

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -2,7 +2,7 @@
 class QuickSort
 {
 public:
-	void swap(int* a, int* b);
+	void swap(double* a, int* b);
 	void quickSort(int array[], int low, int high);
 	int partition(int array[], int low, int high);
 };
