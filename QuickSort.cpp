48688eec9d2772e782bf7a5a8d4d239190c7a205
Themax21210 <118991524+Themax21210@users.noreply.github.com>
2022-11-24 15:08:11 +0000

    Update QuickSort.cpp
    int to double

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,25 +2,25 @@
 
 // Model used for quickSort: https://www.geeksforgeeks.org/quick-sort/
 
-void QuickSort::swap(int* a, int* b)
+void QuickSort::swap(double* a, double* b)
 {
-	int t = *a;
+	double t = *a;
 	*a = *b;
 	*b = t;
 }
 
-void QuickSort::quickSort(int array[], int low, int high)
+void QuickSort::quickSort(double array[], int low, int high)
 {
 	if (low < high) {
-		int partI = partition(array, low, high);
+		double partI = partition(array, low, high);
 		quickSort(array, low, partI - 1);
 		quickSort(array, partI + 1, high);
 	}
 }
 
-int QuickSort::partition(int array[], int low, int high)
+int QuickSort::partition(double array[], int low, int high)
 {
-	int pivot = array[high];
+	double pivot = array[high];
 	int i = low - 1;
 	for (int j = low; j <= high; j++) {
 		if (array[j] < pivot) {
