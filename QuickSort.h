cb7d4c9cacfb644ffa82e98a56672ea8231e5455
Themax21210 <118991524+Themax21210@users.noreply.github.com>
2022-11-24 15:08:43 +0000

    Update QuickSort.h
    Corrected int to double changes

--- QuickSort.h
+++ QuickSort.h
@@ -2,8 +2,8 @@
 class QuickSort
 {
 public:
-	void swap(int* a, int* b);
-	void quickSort(double array[], double low, double high);
-	double partition(double array[], double low, double high);
+	void swap(double* a, double* b);
+	void quickSort(double array[], int low, int high);
+	int partition(double array[], int low, int high);
 };
 
